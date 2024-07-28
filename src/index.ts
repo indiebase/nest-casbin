ac5c652f03caca1c97a489d60196c91ff484a9e7
nawbc <wanghan9423@outlook.com>
2024-07-28 22:52:10 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './access.decorator';
+export * from './actions';
+export * from './casbin.constants';
+export * from './casbin.interface';
+export * from './casbin.module';
+export * from './casbin.service';
+export * from './rpc-authz-client.guard';
